6cc84227c54ca4b2e76342f1a92f91f6d6513631
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-06 11:32:21 +0100

    Tested and implemented an exit station in the journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,8 @@
 class Journey
-  attr_reader :entry_station
-  
+  attr_reader :entry_station, :exit_station
+
   def initialize(params = {})
     @entry_station = params[:entry_station]
+    @exit_station = params[:exit_station]
   end
 end
