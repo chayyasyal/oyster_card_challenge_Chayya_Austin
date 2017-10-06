6e899c0d656aa2026c30ef8cbffd43c0953b41f9
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-06 12:22:22 +0100

    Challenge 14. Added start tests and  method with functionality.

--- lib/journey.rb
+++ lib/journey.rb
@@ -5,4 +5,8 @@ class Journey
     @entry_station = params[:entry_station]
     @exit_station = params[:exit_station]
   end
+
+  def start(entry_station)
+    @entry_station = entry_station
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,6 +5,24 @@ describe Journey do
   let(:entry_station) { double(:entry_station, name: :Victoria, zone: 1) }
   let(:exit_station) { double(:exit_station, name: :Golders_green, zone: 3) }
 
+  let(:journey) { { entry_station: entry_station, exit_station: exit_station } }
+
+  # context '#journey_complete' do
+  #   it 'should show a completed journey' do
+  #     expect(subject.journey).to include(journey)
+  #   end
+  # end
+
+  context '#start' do
+    it 'should add station to entry_station' do
+      expect { subject.start(entry_station) }.to change { subject.entry_station }.to(entry_station)
+    end
+
+  end
+
+  context '#end' do
+
+  end
   context '#entry_station' do
     subject { described_class.new(entry_station: entry_station)}
 
