639b323d730c7a5ef7e10758bbc618c9083fd10d
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-06 12:28:01 +0100

    Tested and implemented an exit station in journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -9,4 +9,8 @@ class Journey
   def start(entry_station)
     @entry_station = entry_station
   end
+
+  def end(exit_station)
+    @exit_station = exit_station
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,7 +1,6 @@
 require './lib/journey'
 
 describe Journey do
-
   let(:entry_station) { double(:entry_station, name: :Victoria, zone: 1) }
   let(:exit_station) { double(:exit_station, name: :Golders_green, zone: 3) }
 
@@ -17,19 +16,19 @@ describe Journey do
     it 'should add station to entry_station' do
       expect { subject.start(entry_station) }.to change { subject.entry_station }.to(entry_station)
     end
-
   end
 
   context '#end' do
-
+    it 'should add station to exit_station' do
+      expect { subject.end(exit_station) }.to change { subject.exit_station }.to(exit_station)
+    end
   end
+
   context '#entry_station' do
-    subject { described_class.new(entry_station: entry_station)}
+    subject { described_class.new(entry_station: entry_station) }
 
     it 'has an entry station' do
       expect(subject.entry_station).to eq entry_station
     end
   end
 end
-
-
